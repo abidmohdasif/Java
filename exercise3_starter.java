8d090392e59f31bd513a5a3e72364f2c74c86c24
Abid <abidmohdasif@gmail.com>
2026-01-14 09:53:01 -0500

    Comments

--- exercise3_starter.java
+++ exercise3_starter.java
@@ -10,16 +10,16 @@ import java.util.Scanner;
 public class exercise3_starter {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-
+// Asks the user for the file
         System.out.print("Enter input file name : ");
         String inputFileName = input.nextLine().trim();
         if (inputFileName.isEmpty()) {
             inputFileName = "Exercise17_14.dat";
         }
-
+// Asks the user for the output file
         System.out.print("Enter output file name (decrypted file): ");
         String outputFileName = input.nextLine().trim();
-
+// gets the output file into a txt
         // Force output to be .txt
         if (!outputFileName.endsWith(".txt")) {
             outputFileName += ".txt";
@@ -29,6 +29,7 @@ public class exercise3_starter {
             BufferedInputStream in = new BufferedInputStream(new FileInputStream(inputFileName));
             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outputFileName))
         ) {
+// the brain of the decryption
             int value;
             while ((value = in.read()) != -1) {
                 out.write(value - 5);
